273f8151760b8cd6dc72028f90668297867573cb
thathalucky <thathalakshman1214@gmail.com>
2025-10-12 11:04:58 +0530

    Add files via upload
    // Make a function which calculates a to the power raised with  b using recursions....

---
+++ powerrec.c
@@ -0,0 +1,34 @@
+// Make a function which calculates a to the power raised with  b using recursions....
+
+#include <stdio.h>
+#include<math.h>
+
+int power ( int a ,int b );
+int power (int a ,int b ){
+    
+ if (b==1){
+     return a*b ;
+   }
+  else {
+
+int add = power(a , b - 1);
+ int total = add*a ;
+      
+  }
+}
+
+
+int main() {
+    
+int  a ;
+printf("Enter the number a  : " );
+scanf("%d" , &a);
+int  b ;
+printf("Enter the number b  : " );
+scanf("%d" , &b);
+
+int power11 = power(a,b);
+printf("%d",power11);
+
+    return 0;
+}
